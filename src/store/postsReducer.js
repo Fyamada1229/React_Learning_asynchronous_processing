e8541c6bbf5316afa94de9d26e16eedd9d382475
Fyamada1229 <yamadafutosi1229@gmail.com>
2022-06-19 23:28:48 +0900

    updata: redux非同期処理追加をする

--- src/store/postsReducer.js
+++ src/store/postsReducer.js
@@ -1,17 +1,33 @@
 import React from "react";
 
-const postsReducer = (
-  state = {
-    posts: [
-      { id: 1, title: "Reduxについて" },
-      {
-        id: 2,
-        title: "ReduxのHooksについて",
-      },
-    ],
-  }
-) => {
-  return state;
+const initialState = {
+  posts: [],
 };
+
+// Redux使用の際に使う
+const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_POST_DATA":
+      return { ...state, posts: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+// Redux使用しない場合
+// const postsReducer = (
+//   state = {
+//     posts: [
+//       { id: 1, title: "Reduxについて" },
+//       {
+//         id: 2,
+//         title: "ReduxのHooksについて",
+//       },
+//     ],
+//   }
+// ) => {
+//   return state;
+// };
 
 export default postsReducer;
