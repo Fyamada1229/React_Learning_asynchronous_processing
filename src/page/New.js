ec1dbd7dc954689f2f7be51d9df864eabe4b102a
Fyamada1229 <yamadafutosi1229@gmail.com>
2022-06-29 09:38:19 +0900

    redux form 追加

--- src/page/New.js
+++ src/page/New.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { connect, useSelector, useDispatch } from "react-redux";
 import { getPosts } from "../store/postsReducer";
+import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
 const New = (props) => {
@@ -10,12 +11,31 @@ const New = (props) => {
     dispatch(getPosts());
   }, [dispatch]);
 
+  const test = (field) => {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error },
+    } = field;
+    return <div></div>;
+  };
+
   return (
     <>
       <div>
-        <h1>welcome</h1>
         <h2>This is New file.</h2>
         <Link to="/">TOP画面</Link>
+        <form>
+          <div>
+            <Field
+              label="Title"
+              name="title"
+              type="text"
+              component={this.test}
+            />
+          </div>
+        </form>
       </div>
     </>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import countReducer from "./countReducer";
 import postsReducer from "./postsReducer";
+import { reducer as reduxFormReducer } from "redux-form";
 
 export default combineReducers({
   countReducer,
   postsReducer,
+  form: reduxFormReducer,
 });
